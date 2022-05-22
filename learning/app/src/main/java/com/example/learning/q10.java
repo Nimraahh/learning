97bcde9821a7440720fc41e60455b6ac3495acf6
Nimraahh <78551921+Nimraahh@users.noreply.github.com>
2022-05-22 16:45:51 +0500

    15th commit

--- learning/app/src/main/java/com/example/learning/q10.java
+++ learning/app/src/main/java/com/example/learning/q10.java
@@ -9,18 +9,22 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class q9 extends AppCompatActivity {
-    Button btn5,btn6,btn7,btn8;
+public class q10 extends AppCompatActivity {
+    Button btn5, btn6, btn7, btn8, btn9;
     TextView txt;
+    TextView txt1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_q9);
-        btn5=(Button)findViewById(R.id.btn30);
-        btn6=(Button)findViewById(R.id.btn31);
-        btn7=(Button)findViewById(R.id.btn32);
-        btn8=(Button)findViewById(R.id.btn33);
-        txt=(TextView) findViewById(R.id.txt2);
+        setContentView(R.layout.activity_q10);
+        btn5 = (Button) findViewById(R.id.btn30);
+        btn6 = (Button) findViewById(R.id.btn31);
+        btn7 = (Button) findViewById(R.id.btn32);
+        btn8 = (Button) findViewById(R.id.btn33);
+        btn9 = (Button) findViewById(R.id.button5);
+        txt = (TextView) findViewById(R.id.txt2);
+        txt1 = (TextView) findViewById(R.id.textView6);
 
         btn5.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,13 +62,9 @@ public class q9 extends AppCompatActivity {
                 btn5.setBackgroundColor(Color.BLACK);
                 btn8.setBackgroundColor(Color.BLACK);
                 txt.setText("");
-
-                btn7.setBackgroundColor(Color.GREEN);
-                txt.setTextColor(Color.GREEN);
-                txt.setText("Correct");
-                Intent intent= new Intent(q9.this,q10.class);
-                startActivity(intent);
-
+                btn7.setBackgroundColor(Color.RED);
+                txt.setTextColor(Color.RED);
+                txt.setText("Wrong");
 
 
             }
@@ -77,12 +77,22 @@ public class q9 extends AppCompatActivity {
                 btn7.setBackgroundColor(Color.BLACK);
                 btn5.setBackgroundColor(Color.BLACK);
                 txt.setText("");
+                btn8.setBackgroundColor(Color.GREEN);
+                txt.setTextColor(Color.GREEN);
+                txt.setText("Correct");
+                txt1.setTextColor(Color.GREEN);
+                txt1.setText("Congratulations on finishing your Exam!");
+                btn9.getVisibility();
+                btn9.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(q10.this, MainActivity.class);
+                        startActivity(intent);
 
-                btn8.setBackgroundColor(Color.RED);
-                txt.setTextColor(Color.RED);
-                txt.setText("Wrong");
+                    }
+                });
+
             }
         });
-
     }
 }
